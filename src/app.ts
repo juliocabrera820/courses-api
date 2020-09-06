076d447c95fe78fb21562e04272cfd14f0b36a57
juliocabrera820 <juliocabrera820@gmail.com>
2020-09-06 02:33:52 -0500

    feat: version REST API

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata'
 import express, { json } from 'express'
 import { createConnection } from 'typeorm'
 import cors from 'cors'
+import routes from './routes'
 
 createConnection()
 const app = express()
 
 app.use(cors())
 app.use(json())
+app.use('/api/v1', routes)
 
 export default app
